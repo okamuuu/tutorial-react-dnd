c2c21559ac3cd6a4e01a1509d8b2af3273e0e5b8
okamuuu <okamuuu@gmail.com>
2018-05-23 23:07:09 +0900

    make component droppable

--- src/Game.js
+++ src/Game.js
@@ -6,7 +6,7 @@ import Board from './components/Board';
 import Box from './components/Box';
 import Piece from './components/Piece';
 
-import { DragDropContext, DragSource } from 'react-dnd';
+import { DragDropContext, DragSource, DropTarget } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
 const state = store({ position: 0 });
@@ -28,7 +28,51 @@ const Types = {
 // Board
 const DndBoard = DragDropContext(HTML5Backend)(Board);
 
-// Board
+// Box
+const dropTarget = {
+  drop(props, monitor) {
+    const { position } = props;
+    state.position = position;
+    return {};
+  },
+  canDrop(props, monitor) {
+    return canMove(props.position);
+  }
+};
+
+const ConnectedTarget = props => {
+  const {canDrop, children, connectDropTarget} = props;
+  const renderOverlay = (color) => {
+    return (
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        zIndex: 1,
+        opacity: 0.5,
+        backgroundColor: color,
+      }} />
+    );
+  }
+  return (
+    <Box {...props} innerRef={instance=>connectDropTarget(instance)}>
+      {children}
+      { !children && canDrop &&
+        <div style={{position: 'relative', width: '100%', height: '100%'}}>
+          {renderOverlay('yellow')}
+        </div>
+      }
+    </Box>
+  )
+}
+
+const DndBox = DropTarget(Types.PIECE, dropTarget, (connect, monitor) => {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    canDrop: monitor.canDrop(),
+}})(ConnectedTarget);
 
 // Piece
 const dragSource = {
@@ -56,7 +100,7 @@ class Game extends Component {
   render() {
     const boxes = _.times(9, n => {
       const piece = state.position === n ? <DndPiece /> : null;
-      return (<Box onClick={() => move(n)} key={n} position={n}>{piece}</Box>)
+      return (<DndBox onClick={() => move(n)} key={n} position={n}>{piece}</DndBox>)
     });
 
     return (
